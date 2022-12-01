336aacc2b164923b0ea6afb7e443c97ca149e084
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2022-12-01 14:49:05 -0700

    part 2 answer

--- day/1/index.js
+++ day/1/index.js
@@ -30,19 +30,46 @@ const makeGroups = array => {
 	// Create groups
 	const groups = makeGroups(input);
 
+	const countTotalCaloriesCarried = itemsCarried =>
+		itemsCarried.reduce((prev, curr) => {
+			return (prev += parseInt(curr));
+		}, 0);
+
 	/* Count the total number of calories per group.
 	If the total number of calories exceeds the previous total number of calories,
 	make that the largest total until the entire list has been evaluated. */
 	let largestTotalCarriedCalories = 0;
-	const handleGroup = itemsCarried => {
-		const totalCaloriesCarried = itemsCarried.reduce((prev, curr) => {
-			return (prev += parseInt(curr));
-		}, 0);
+	const findLargestTotalCaloriesCarried = itemsCarried => {
+		const totalCaloriesCarried = countTotalCaloriesCarried(itemsCarried);
 		if (totalCaloriesCarried > largestTotalCarriedCalories)
 			largestTotalCarriedCalories = totalCaloriesCarried;
 	};
 
 	// Iterate the groups and find the largest total number of calories
-	for (const group of groups) handleGroup(group);
+	for (const group of groups) findLargestTotalCaloriesCarried(group);
+
+	// Part 1 Answer:
 	console.log(largestTotalCarriedCalories);
+
+	/* Count the total number of calories carried in a given group to find the top three
+	greatest total of calories carried by any given elf */
+	let topThree = [0, 0, 0];
+	const findTopThreeLargestCalories = itemsCarried => {
+		const totalCaloriesCarried = countTotalCaloriesCarried(itemsCarried);
+		topThree = topThree.sort((a, b) => a - b);
+		if (totalCaloriesCarried > topThree[0])
+			topThree[0] = totalCaloriesCarried;
+	};
+
+	// Iterate the groups to find the top three largest calories carried
+	for (const group of groups) findTopThreeLargestCalories(group);
+
+	// Count the total number of calories carried by the top three
+	const totalCarriedBetweenTopThree = topThree.reduce(
+		(prev, curr) => (prev += curr),
+		0
+	);
+
+	// Part 2 Answer:
+	console.log(totalCarriedBetweenTopThree);
 })();
