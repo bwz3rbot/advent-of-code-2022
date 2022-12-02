2e7170012cbb3722d58e1119c0bd30c9fb487b8b
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2022-12-02 12:40:26 -0700

    add comments

--- day/2/index.js
+++ day/2/index.js
@@ -1,10 +1,15 @@
+// read the input.txt file
 const getInput = require("../../util/getInput");
+
+// rock/paper/scissors (R/P/S)
 const TYPE = {
 	ROCK: { NAME: "ROCK", SCORE: 1, BEATS: "SCISSORS", LOSES: "PAPER" },
 	PAPER: { NAME: "PAPER", SCORE: 2, BEATS: "ROCK", LOSES: "SCISSORS" },
 	SCISSORS: { NAME: "SCISSORS", SCORE: 3, BEATS: "PAPER", LOSES: "ROCK" },
 };
 
+// used for part 1 before the stinky elf ran off without giving me complete directions!
+// *its okay, we can still use it for part 2*
 const getRockPaperScissors = letter => {
 	switch (letter) {
 		case "A":
@@ -18,10 +23,14 @@ const getRockPaperScissors = letter => {
 			return TYPE.SCISSORS;
 	}
 };
+
+// nice little helper method to search an object by key name
 const findTypeByName = name => {
 	const entries = Object.entries(TYPE);
 	return entries.find(([k, v]) => k === name)[1];
 };
+
+// used for part 2 in finding which type(R/P/S) I should throw for a given match
 const getMyTurnType = (opponent, mySignal) => {
 	switch (mySignal) {
 		case "X":
@@ -35,6 +44,7 @@ const getMyTurnType = (opponent, mySignal) => {
 	}
 };
 
+// scoring as outlined here: https://adventofcode.com/2022/day/2
 const getTurnScore = (a, b) => {
 	if (a.NAME === b.NAME) {
 		return [3 + a.SCORE, 3 + b.SCORE];
@@ -43,6 +53,7 @@ const getTurnScore = (a, b) => {
 	if (a.LOSES === b.NAME) return [a.SCORE, b.SCORE + 6];
 };
 
+// calculate my total score for a list of matches
 const getMyScore = strategy => {
 	const scores = strategy.map(match => getTurnScore(match[0], match[1]));
 	const myTotalScore = scores.reduce((prev, score) => {
@@ -50,8 +61,11 @@ const getMyScore = strategy => {
 	}, 0);
 	return myTotalScore;
 };
+
 getInput(2).then(i => {
 	const input = i.split("\n");
+
+	// create a list of turns (p1:R/P/S, p2:R/P/S)
 	const strategy = input.map(match => {
 		const playerTurns = match.split(" ");
 		return [
@@ -60,10 +74,11 @@ getInput(2).then(i => {
 		];
 	});
 
-	const myTotalScore = getMyScore(strategy);
 	// Part 1 answer:
+	const myTotalScore = getMyScore(strategy);
 	console.log(myTotalScore);
 
+	// create a list of turns with the CORRECT strategy (part 2)
 	const correctStrategy = input.map((match, i) => {
 		const playerTurns = match.split(" ");
 		const opponent = getRockPaperScissors(playerTurns[0]);
@@ -71,6 +86,7 @@ getInput(2).then(i => {
 		return [opponent, myTurn];
 	});
 
+	// Part 2 answer:
 	const myCorrectTotalScore = getMyScore(correctStrategy);
 	console.log(myCorrectTotalScore);
 });
